44804c9afd8266b266fcea9827993c5e12e42aeb
anakwon <anakwon@gmail.com>
2015-04-11 16:55:36 -0700

    testing tech prototypes

---
+++ prototypes/tech_proto.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Tech Prototype</title>
+</head>
+<style>
+	.container{
+		height:200px;
+		width:200px;
+		background-color: lavender;
+	}
+	.title{
+		font-size:20px;
+		text-align: center;
+		height:20px;
+		width:auto;
+	}
+	.lang{
+		height:200px;
+		width:80px;
+		background-color: white;
+	}
+	li{
+		text-align: center;
+		list-style-type: none;
+		padding:10px;
+	}
+	.frontend:hover {
+		font-size:25px;
+		text-decoration: none;
+	}
+</style>
+<body>
+<div class="container">
+	<div class="title">
+		Front End Developer
+	</div>
+	<ul class="lang">
+		<a href="www.github.com/anakwon/digital_resume" title="Example of JavaScript" target="_blank" class="frontend">
+		<li>JavaScript</a></li>
+		<li>CSS 3</li>
+		<li>HTML 5</li>
+		<li>jQuery</li>
+	</ul>
+</div>
+
+</body>
+
+</html>
+
